6e04c69ab856401984ac72b5652e42d7f210ae7d
saurya3d <saurabhchvn9200@gmail.com>
2021-01-13 19:04:22 +0530

    1

---
+++ my_app/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+class Search(models.Model):
+    search = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return '{}'.format(self.search)
+
